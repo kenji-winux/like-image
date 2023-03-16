da2611a20b698e1bcf21c463fdbd20e25c7988e9
Ma Hoang Minh <68465390+kenji-winux@users.noreply.github.com>
2023-03-16 18:01:52 +0700

    Update GetMitreData.ps1

--- GetMitreData.ps1
+++ GetMitreData.ps1
@@ -2,13 +2,16 @@
 $techniqueID = Read-Host "Enter Mitre ATT&CK Technique ID"
 
 # Construct the API URL for retrieving the data components for the specified technique ID
-$url = "https://mitre-attack.github.io/attack-navigator/enterprise-layer/techniques/" + $techniqueID + ".json"
+$url = "https://cti-taxii.mitre.org/stix/collections/enterprise-attack--4e2e2b6e-0e4b-4ea4-9ac4-aa04c14eeeb3/objects?match[type]=attack-pattern&match[external_references.external_id]=$techniqueID"
+
+# Set the headers for the API request
+$headers = @{"User-Agent"="PowerShell"}
 
 # Invoke the API to retrieve the data components for the specified technique ID
-$data = Invoke-RestMethod $url
+$data = Invoke-RestMethod -Uri $url -Headers $headers
 
 # Extract the data components from the retrieved JSON data
-$dataComponents = $data."x_mitre_data_components"
+$dataComponents = $data.objects.properties.x_mitre_data_components
 
 # Output the data components
 Write-Output "Data Components for Technique $techniqueID:`n"
