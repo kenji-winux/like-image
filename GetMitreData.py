7e2a36995843a69ffe69145bf69a0c57bb2a9356
Ma Hoang Minh <68465390+kenji-winux@users.noreply.github.com>
2023-03-17 13:34:33 +0700

    Update GetMitreData.py

--- GetMitreData.py
+++ GetMitreData.py
@@ -1,3 +1,36 @@
+import openpyxl
+import re
+
+# Specify the path to the Excel file
+file_path = 'path/to/excel_file.xlsx'
+
+# Load the workbook
+workbook = openpyxl.load_workbook(file_path)
+
+# Select the worksheet you want to extract data from
+worksheet = workbook.active
+
+# Define a set to hold the unique MITRE technique IDs found
+technique_ids = set()
+
+# Define the regular expression pattern to match MITRE technique IDs
+pattern = r'T\d{4}'
+
+# Loop through each row in column C, starting from the second row
+for row in worksheet.iter_rows(min_row=2, min_col=3, max_col=3):
+    cell = row[0]
+    # Extract all the MITRE technique IDs from the cell using the regex pattern
+    matches = re.findall(pattern, cell.value)
+    # Add the technique IDs to the set
+    technique_ids.update(matches)
+
+# Print the list of technique IDs
+print(list(technique_ids))
+
+
+
+
+
 import requests
 from bs4 import BeautifulSoup
 import openpyxl
