6acb40b5d3244540ae17776903623ca2bc26c200
Ma Hoang Minh <68465390+kenji-winux@users.noreply.github.com>
2023-03-17 11:52:22 +0700

    Update GetMitreData.py

--- GetMitreData.py
+++ GetMitreData.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import openpyxl
 
 # specify the URL of the website to scrape
 url = 'https://www.example.com'
@@ -16,12 +17,19 @@ soup = BeautifulSoup(response.content, 'html.parser')
 # find the table with the specified class using the find() method
 table = soup.find('table', {'class': table_class})
 
+# create a new Excel workbook and worksheet
+workbook = openpyxl.Workbook()
+worksheet = workbook.active
+
 # check if the table was found
 if table is not None:
-    # iterate over each row in the table and print the second and third columns
+    # iterate over each row in the table and write the second and third columns to the worksheet
     for row in table.find_all('tr'):
         cells = row.find_all('td')
         if len(cells) >= 3:
-            print(cells[1].text + '/' + cells[2].text)
+            worksheet.append([cells[1].text, cells[2].text])
 else:
     print('Table not found on the page.')
+
+# save the Excel workbook to a file
+workbook.save('output.xlsx')
