43b2e621ac40f6cebfff4a04f5af23f2fa7ed9dc
Ma Hoang Minh <68465390+kenji-winux@users.noreply.github.com>
2023-03-17 11:28:21 +0700

    Update GetMitreData.py

--- GetMitreData.py
+++ GetMitreData.py
@@ -4,6 +4,9 @@ from bs4 import BeautifulSoup
 # specify the URL of the website to scrape
 url = 'https://www.example.com'
 
+# specify the class name of the table to extract
+table_class = 'my-table-class'
+
 # download the HTML page using the requests library
 response = requests.get(url)
 
@@ -11,15 +14,14 @@ response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
 
 # find the table with the specified class using the find() method
-table = soup.find('table', {'class': 'my-table-class'})
+table = soup.find('table', {'class': table_class})
 
 # check if the table was found
 if table is not None:
-    # iterate over each row in the table and print the data
+    # iterate over each row in the table and print the second and third columns
     for row in table.find_all('tr'):
-        # iterate over each cell in the row and print the data
-        for cell in row.find_all('td'):
-            print(cell.text, end='\t')
-        print()
+        cells = row.find_all('td')
+        if len(cells) >= 3:
+            print(cells[1].text, cells[2].text)
 else:
     print('Table not found on the page.')
