bb6ca75d045e706efc0c393af74499ae64fc4aed
Ma Hoang Minh <68465390+kenji-winux@users.noreply.github.com>
2023-03-17 14:26:06 +0700

    Update GetMitreData.py

--- GetMitreData.py
+++ GetMitreData.py
@@ -16,7 +16,6 @@ for technique_id in technique_ids:
 
 
 
-
 import openpyxl
 import re
 
@@ -29,22 +28,25 @@ workbook = openpyxl.load_workbook(file_path)
 # Select the worksheet you want to extract data from
 worksheet = workbook.active
 
-# Define a set to hold the unique MITRE technique IDs found
-technique_ids = set()
+# Define a list to hold the technique and sub-technique IDs found
+technique_ids = []
 
-# Define the regular expression pattern to match MITRE technique IDs
-pattern = r'T\d{4}'
+# Define the regular expression pattern to match MITRE technique and sub-technique IDs
+pattern = r'(T\d{4})\.(\d{1,2})'
 
-# Loop through each row in column C, starting from the second row
-for row in worksheet.iter_rows(min_row=2, min_col=3, max_col=3):
-    cell = row[0]
-    # Extract all the MITRE technique IDs from the cell using the regex pattern
-    matches = re.findall(pattern, cell.value)
-    # Add the technique IDs to the set
-    technique_ids.update(matches)
+# Loop through each cell in the worksheet
+for row in worksheet.iter_rows(min_row=1, min_col=1, max_col=worksheet.max_column, values_only=True):
+    for cell in row:
+        if cell is not None:
+            # Extract the sub-technique ID from the cell using the regex pattern
+            match = re.search(pattern, cell)
+            if match:
+                technique_ids.append(match.group(0))
 
-# Print the list of technique IDs
-print(list(technique_ids))
+# Print the list of technique and sub-technique IDs
+print(technique_ids)
+
+
 
 
 
