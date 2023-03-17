809ab8fe5cbf28b8c7dbe3baab2e8d7da7c8db21
Ma Hoang Minh <68465390+kenji-winux@users.noreply.github.com>
2023-03-17 15:17:15 +0700

    Update GetMitreData.py

--- GetMitreData.py
+++ GetMitreData.py
@@ -1,24 +1,22 @@
 import requests
-import pandas as pd
-import openpyxl
-import pyperclip
 from bs4 import BeautifulSoup
+from tabulate import tabulate
 
 while True:
-    technique_id = input("Enter a MITRE ATT&CK technique ID (e.g., T1059): ")
-    if technique_id == "T000":
+    technique_id = input("Enter a MITRE ATT&CK technique ID (enter T0000 to exit): ")
+    if technique_id == "T0000":
         break
     url = f"https://attack.mitre.org/techniques/{technique_id}/"
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
-    table = soup.find('table', {'class': 'table table-bordered table-hover table-condensed'})
-    if table:
-        df = pd.read_html(str(table))[0].iloc[:, 1:3]
-        filename = f"{technique_id}.xlsx"
-        with pd.ExcelWriter(filename, mode='w', engine='openpyxl') as writer:
-            df.to_excel(writer, index=False, header=False)
-            writer.save()
-        pyperclip.copy(df.to_string(index=False, header=False))
-        print(f"Data for {technique_id} has been copied to the clipboard and saved to {filename}.")
-    else:
-        print(f"No data found for {technique_id}.")
+    table = soup.find('table', {'class': 'table table-bordered table-hover table-striped'})
+    if table is None:
+        print("No table found for this technique ID.")
+        continue
+    headers = [th.text.strip() for th in table.find_all('th')]
+    data = []
+    for row in table.find_all('tr'):
+        cols = row.find_all('td')
+        if len(cols) >= 3:
+            data.append([cols[1].text.strip(), cols[2].text.strip()])
+    print(tabulate(data, headers=headers[1:]))
