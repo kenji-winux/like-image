83133173d0cd163fdff2575efce57c82cb4cc446
Ma Hoang Minh <68465390+kenji-winux@users.noreply.github.com>
2023-03-17 13:56:43 +0700

    Update GetMitreData.py

--- GetMitreData.py
+++ GetMitreData.py
@@ -48,46 +48,34 @@ print(list(technique_ids))
 
 
 
-
-
-import requests
-from bs4 import BeautifulSoup
 import openpyxl
+import re
 
-# specify the class name of the table to extract
-table_class = 'my-table-class'
+# Specify the path to the Excel file
+file_path = 'path/to/excel_file.xlsx'
 
-# create a new Excel workbook and worksheet
-workbook = openpyxl.Workbook()
+# Load the workbook
+workbook = openpyxl.load_workbook(file_path)
+
+# Select the worksheet you want to extract data from
 worksheet = workbook.active
 
-# iterate over each URL in the list
-urls = ['https://www.example.com/page1', 'https://www.example.com/page2', 'https://www.example.com/page3']
-for url in urls:
-    # download the HTML page using the requests library
-    response = requests.get(url)
+# Define a set to hold the unique MITRE technique IDs found
+technique_ids = set()
 
-    # parse the HTML page using the BeautifulSoup library
-    soup = BeautifulSoup(response.content, 'html.parser')
+# Define the regular expression pattern to match MITRE technique IDs
+pattern = r'T\d{4}'
 
-    # find the table with the specified class using the find() method
-    table = soup.find('table', {'class': table_class})
+# Loop through each row in column C, starting from the second row
+for row in worksheet.iter_rows(min_row=2, min_col=3, max_col=3):
+    cell = row[0]
+    if cell.value is not None:  # Skip over empty cells
+        # Extract all the MITRE technique IDs from the cell using the regex pattern
+        matches = re.findall(pattern, cell.value)
+        # Add the technique IDs to the set
+        technique_ids.update(matches)
 
-    # write a blank line to separate the output from different URLs
-    worksheet.append([])
+# Print the list of technique IDs
+print(list(technique_ids))
 
-    # write the URL to the worksheet
-    worksheet.append(['URL:', url])
 
-    # check if the table was found
-    if table is not None:
-        # iterate over each row in the table and write the second and third columns to the worksheet
-        for row in table.find_all('tr'):
-            cells = row.find_all('td')
-            if len(cells) >= 3:
-                worksheet.append([cells[1].text, cells[2].text])
-    else:
-        worksheet.append(['Table not found on the page.'])
-
-# save the Excel workbook to a file
-workbook.save('output.xlsx')
