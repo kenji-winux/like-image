056658594b986a522da02e5f0fed197c5d1bf6f5
Ma Hoang Minh <68465390+kenji-winux@users.noreply.github.com>
2023-03-17 13:42:59 +0700

    Update GetMitreData.py

--- GetMitreData.py
+++ GetMitreData.py
@@ -54,34 +54,40 @@ import requests
 from bs4 import BeautifulSoup
 import openpyxl
 
-# specify the URL of the website to scrape
-url = 'https://www.example.com'
-
 # specify the class name of the table to extract
 table_class = 'my-table-class'
-
-# download the HTML page using the requests library
-response = requests.get(url)
-
-# parse the HTML page using the BeautifulSoup library
-soup = BeautifulSoup(response.content, 'html.parser')
-
-# find the table with the specified class using the find() method
-table = soup.find('table', {'class': table_class})
 
 # create a new Excel workbook and worksheet
 workbook = openpyxl.Workbook()
 worksheet = workbook.active
 
-# check if the table was found
-if table is not None:
-    # iterate over each row in the table and write the second and third columns to the worksheet
-    for row in table.find_all('tr'):
-        cells = row.find_all('td')
-        if len(cells) >= 3:
-            worksheet.append([cells[1].text, cells[2].text])
-else:
-    print('Table not found on the page.')
+# iterate over each URL in the list
+urls = ['https://www.example.com/page1', 'https://www.example.com/page2', 'https://www.example.com/page3']
+for url in urls:
+    # download the HTML page using the requests library
+    response = requests.get(url)
+
+    # parse the HTML page using the BeautifulSoup library
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # find the table with the specified class using the find() method
+    table = soup.find('table', {'class': table_class})
+
+    # write a blank line to separate the output from different URLs
+    worksheet.append([])
+
+    # write the URL to the worksheet
+    worksheet.append(['URL:', url])
+
+    # check if the table was found
+    if table is not None:
+        # iterate over each row in the table and write the second and third columns to the worksheet
+        for row in table.find_all('tr'):
+            cells = row.find_all('td')
+            if len(cells) >= 3:
+                worksheet.append([cells[1].text, cells[2].text])
+    else:
+        worksheet.append(['Table not found on the page.'])
 
 # save the Excel workbook to a file
 workbook.save('output.xlsx')
