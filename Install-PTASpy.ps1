41b331defad60f4ab8642bbb8fcf4f77b89b34f7
Ma Hoang Minh <68465390+kenji-winux@users.noreply.github.com>
2024-08-01 11:03:32 +0700

    Update Install-PTASpy.ps1

--- Install-PTASpy.ps1
+++ Install-PTASpy.ps1
@@ -3,7 +3,7 @@
 Install-Module aadinternals -Confirm:$false -Force
 Import-Module aadinternals
 
-curl https://raw.githubusercontent.com/kenji-winux/public-folder/main/PTASpy.ps1 -o "C:\Program Files\WindowsPowerShell\Modules\AADInternals\0.9.3\PTASpy.ps1"
+curl https://raw.githubusercontent.com/kenji-winux/public-folder/main/PTASpy.ps1 -o "C:\Program Files\WindowsPowerShell\Modules\AADInternals\0.9.4\PTASpy.ps1"
 Import-Module aadinternals -Force
 
 
