a11cbc52367aa017daae6f08d8904f2db4fb5b2e
Ma Hoang Minh <68465390+kenji-winux@users.noreply.github.com>
2024-08-01 11:29:38 +0700

    Update Install-PTASpy.ps1

--- Install-PTASpy.ps1
+++ Install-PTASpy.ps1
@@ -1,9 +1,9 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-Install-Module aadinternals -Confirm:$false -Force
+Install-Module AADInternals -RequiredVersion 0.9.3 -Scope AllUsers -Force
 Import-Module aadinternals
 
-curl https://raw.githubusercontent.com/kenji-winux/public-folder/main/PTASpy.ps1 -o "C:\Program Files\WindowsPowerShell\Modules\AADInternals\0.9.4\PTASpy.ps1"
+curl https://raw.githubusercontent.com/kenji-winux/public-folder/main/PTASpy.ps1 -o "C:\Program Files\WindowsPowerShell\Modules\AADInternals\0.9.3\PTASpy.ps1"
 Import-Module aadinternals -Force
 
 
